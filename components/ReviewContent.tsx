50a1bc6b972ccbeff78b9323b649a263c828e9d4
Olga Blud <olga.blud@gmail.com>
2025-11-04 12:13:32 +0200

    implemented nativewind

--- components/ReviewContent.tsx
+++ components/ReviewContent.tsx
@@ -6,6 +6,11 @@ function ReviewContent() {
     <View style={{ marginBottom: 16 }}>
       <Text>Reviews List</Text>
       <Text>Содержимое Review...</Text>
+      <View className="flex-1 items-center justify-center bg-blue">
+        <Text className="text-xl font-bold text-blue-500">
+          Welcome to Nativewind!
+        </Text>
+      </View>
     </View>
   );
 }
